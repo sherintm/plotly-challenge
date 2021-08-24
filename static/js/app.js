b04e2b51a2c3a6cd4d70fe0d8d7f3d066a7802a7
Sherin Thomas <sherinmt@gmail.com>
2021-08-24 17:07:30 +1000

    Added gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,56 +1,4 @@
-// Use d3.json() to fetch data from JSON file
-// Incoming data is internally referred to as incomingData
-// d3.json("../../samples.json").then((incomingData) => {
-    // function filterMovieRatings(movie) {
-    //   return movie.imdbRating > 8.9;
-   // console.log(incomingData)
-    // }
-  
-    // // Use filter() to pass the function as its argument
-    // var filteredMovies = incomingData.filter(filterMovieRatings);
-  
-    // //  Check to make sure your are filtering your movies.
-    // console.log(filteredMovies);
-  
-    // // Use the map method with the arrow function to return all the filtered movie titles.
-    // var titles = filteredMovies.map(movies =>  movies.title);
-  
-    // // Use the map method with the arrow function to return all the filtered movie metascores.
-    // var ratings = filteredMovies.map(movies => movies.metascore);
-  
-    // // Check your filtered metascores.
-    // console.log(ratings);
-  
-    // // Create your trace.
-    // var trace = {
-    //   x: titles,
-    //   y: ratings,
-    //   type: "bar"
-    // };
-  
-    // // Create the data array for our plot
-    // var data = [trace];
-  
-    // // Define the plot layout
-    // var layout = {
-    //   title: "The highest critically acclaimed movies.",
-    //   xaxis: { title: "Title" },
-    //   yaxis: { title: "Metascore (Critic) Rating"}
-    // };
-  
-    // // Plot the chart to a div tag with id "bar-plot"
-//     // Plotly.newPlot("bar-plot", data, layout);
-//   });
-  
-
-//   // Create an array of each country's numbers
-// var us = Object.values(data.us);
-// var uk = Object.values(data.uk);
-// var canada = Object.values(data.canada);
-
-// // Create an array of music provider labels
-// var labels = Object.keys(data.us);
-
+// App.js
 let dropDown = d3.select('#selDataset');
 let dropDownData;
 let jsonData;
@@ -58,19 +6,18 @@ let jsonData;
 d3.json("../../samples.json").then((incomingData) => {
     jsonData = incomingData;
     dropDownData = incomingData.names;
-    //console.log(dropDownData);
     dropDownData.forEach(element => {
         var options = dropDown.append("option");
         options.text(element);
         options.attr("value",element);
-        //console.log("Elemet" + element)
     });
+    optionChanged(jsonData.names[0])
 });
 
 function optionChanged(value)
 {
-    console.log(value);
     let row = jsonData.metadata.filter(element => (element.id == value));
+    // Demographic information for selected option
     d3.select('#sample-metadata').html("ID: " + row[0].id +
                                        "<br> ETHNICITY: " + row[0].ethnicity +
                                        "<br> GENDER: " + row[0].gender +
@@ -82,7 +29,7 @@ function optionChanged(value)
     var samples = jsonData.samples.filter(element => (element.id == value));
     var otu_ids = samples[0].otu_ids.slice(0, 10).map(element => "OTU" + element);
     console.log(otu_ids);
-    var trace = {
+    var barTrace = {
         x:samples[0].sample_values.slice(0, 10),
         y: otu_ids,
         text: samples[0].otu_labels.slice(0, 10),
@@ -90,55 +37,69 @@ function optionChanged(value)
         orientation: 'h'
       };
       // 6. Create the data array for our plot
-      var data = [trace];
+      var barData = [barTrace];
             
       // 7. Define our plot layout
-      var layout = {
+      var barLayout = {
         title: "Top 10 Bacteria Cultures Found",
         yaxis: {autorange:"reversed"}
       };
       
       // 8. Plot the chart to a div tag with id "bar-plot"
-      Plotly.newPlot("bar", data, layout);
+      Plotly.newPlot("bar", barData, barLayout);
+
+      // Belly button washing frequency
+      var gaugeData = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: row[0].wfreq,
+          title: { text: "Belly Button Washing Frquency<br><br>Scrubs per Week" },
+          type: "indicator",
+          mode: "gauge+number",
+          gauge: {
+            axis: { range: [null, 9] },
+            steps: [
+              { range: [0, 1], color: "#d6f5f5" },
+              { range: [1, 2], color: "#c2f0f0" },
+              { range: [2, 3], color: "#adebeb" },
+              { range: [3, 4], color: "#99e6e6" },
+              { range: [4, 5], color: "#85e0e0" },
+              { range: [5, 6], color: "#70dbdb" },
+              { range: [6, 7], color: "#5cd6d6" },
+              { range: [7, 8], color: "#47d1d1" },
+              { range: [8, 9], color: "#33cccc" }
+              
+            ],
+            threshold: {
+              line: { color: "purple", width: 4 },
+              thickness: 0.75,
+              value: row[0].wfreq
+            }
+          }
+        }
+      ];
+      
+      Plotly.newPlot('gauge', gaugeData);
+
+      // Bubble cultures per sample 
+      let bubbleTrace = {
+        x: samples[0].otu_ids,
+        y: samples[0].sample_values,
+        text: samples[0].otu_labels,
+        mode: 'markers',
+        marker: {
+          color: samples[0].otu_ids,
+          size: samples[0].sample_values
+        }
+      };
+      
+      let bubbleData = [bubbleTrace];
+      
+      let bubbleLayout = {
+        title: 'Bacteria Cultures Per Sample',
+        xaxis: {'title':'OTU ID'},
+        showlegend: false,
+      };
+      
+      Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 }
-
-
-
-
-// Display the default plots
-function init() {
-
-
-  //Plotly.newPlot("pie", data, layout);
-};
-
-// On change to the DOM, call getData()
-//3.selectAll("#selDataset").on("change", getData);
-
-// Function called by DOM changes
-// function getData() {
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-//   // Initialize an empty array for the country's data
-//   var data = [];
-
-//   if (dataset == 'us') {
-//       data = us;
-//   }
-//   else if (dataset == 'uk') {
-//       data = uk;
-//   }
-//   else if (dataset == 'canada') {
-//       data = canada;
-//   }
-//   // Call function to update the chart
-//   updatePlotly(data);
-// }
-
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("pie", "values", [newdata]);
-// }
-
-init();
